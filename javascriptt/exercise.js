dea7b69f07b407b12cd700cb3a2950205a3de3a6
MichaelKumanov <michaelkumanov@gmail.com>
2024-11-15 03:05:05 +0200

    Add files via upload

---
+++ javascriptt/exercise.js
@@ -0,0 +1,15 @@
+
+// 1
+let myfullname = {
+  Fullname: "Michael Kumanov",
+  Age: 23,
+};
+
+// 3
+console.log(myfullname);
+const date = "11.09.2001";
+console.log(date);
+
+//2
+let age = 23;
+age = 18;
